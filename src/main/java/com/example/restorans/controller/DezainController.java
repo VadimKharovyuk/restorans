0f7ee301fea05e4d0172d7da439c7e22c06c08d4
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-03-13 16:30:36 +0530

    каусель

---
+++ src/main/java/com/example/restorans/controller/DezainController.java
@@ -0,0 +1,12 @@
+package com.example.restorans.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DezainController {
+    @GetMapping("/dezain")
+    public String dezzin(){
+        return "dezain";
+    }
+}
