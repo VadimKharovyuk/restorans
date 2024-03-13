cf436c6505be6fe8f371f4582d1655ca6947535a
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-03-13 12:14:44 +0530

    Калькулятор заказа done

--- src/main/java/com/example/restorans/controller/BasketController.java
+++ src/main/java/com/example/restorans/controller/BasketController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.math.BigDecimal;
+
 @Controller
 @AllArgsConstructor
 public class BasketController {
@@ -25,42 +27,41 @@ public class BasketController {
     private final ChildrenEatRepository childrenEatRepository;
 
 
-
-
-
-
     //корзина
     @GetMapping("/mylist")
-    public String basket(Model model){
-        model.addAttribute("order",basketService.findAllBasketList(new Basket()));
+    public String basket(Model model) {
+        model.addAttribute("order", basketService.findAllBasketList(new Basket()));
+            BigDecimal totalCost = basketService.calculateTotalOrderCost();
+            model.addAttribute("totalCost", totalCost);
+
         return "basketList";
     }
-
 
 
     //кофе
     @RequestMapping("/mylist/{id}")
     public String AddMyBasketList(@PathVariable(name = "id") Long id) {
         Coffee coffee = coffeeService.getCoffeeById(id);
-        Basket basketlist=new Basket(coffee.getId(),coffee.getName(),coffee.getPrice());
+        Basket basketlist = new Basket(coffee.getId(), coffee.getName(), coffee.getPrice());
         basketService.saveMyorder(basketlist);
         return "redirect:/menu";
 
     }
+
     //первые блюда
     @RequestMapping("/mylist/pervieBludo/{id}")
-    public String addToBusketFirstBludo(@PathVariable (name = "id") Long id){
+    public String addToBusketFirstBludo(@PathVariable(name = "id") Long id) {
         PervieBludo pervieBludo = pervieBludoRepositoty.getReferenceById(id);
-        Basket basket = new Basket(pervieBludo.getId(),pervieBludo.getName(),pervieBludo.getPrice());
+        Basket basket = new Basket(pervieBludo.getId(), pervieBludo.getName(), pervieBludo.getPrice());
         basketService.saveMyorder(basket);
         return "redirect:/menu";
     }
 
     //десерты
     @RequestMapping("/mylist/desert/{id}")
-    public String addToBusketDesert(@PathVariable (name = "id") Long id){
-        Dessert dessert =  dessertRepository.getReferenceById(id);
-        Basket basketDesert = new Basket(dessert.getId(),dessert.getName(),dessert.getPrice());
+    public String addToBusketDesert(@PathVariable(name = "id") Long id) {
+        Dessert dessert = dessertRepository.getReferenceById(id);
+        Basket basketDesert = new Basket(dessert.getId(), dessert.getName(), dessert.getPrice());
         basketService.saveMyorder(basketDesert);
         return "redirect:/menu";
     }
@@ -68,31 +69,31 @@ public class BasketController {
 
     //алкоголь
     @RequestMapping("/mylist/alcohol/{id}")
-    public String addBusketAlkohol(@PathVariable (name = "id") Long id){
+    public String addBusketAlkohol(@PathVariable(name = "id") Long id) {
         Alcohol alcohol = alcoholRepository.getReferenceById(id);
-        Basket basketAlkohol = new Basket(alcohol.getId(),alcohol.getName(),alcohol.getPrice());
+        Basket basketAlkohol = new Basket(alcohol.getId(), alcohol.getName(), alcohol.getPrice());
         basketService.saveMyorder(basketAlkohol);
         return "redirect:/menu";
 
     }
+
     //детское питание
     @RequestMapping("/mylist/childredEat/{id}")
-    public String addBusketChildrenFood(@PathVariable (name = "id") Long id){
+    public String addBusketChildrenFood(@PathVariable(name = "id") Long id) {
         ChildrenEat childrenEat = childrenEatRepository.getReferenceById(id);
-        Basket basketChildrenFood = new Basket(childrenEat.getId(),childrenEat.getName(),childrenEat.getPrice());
+        Basket basketChildrenFood = new Basket(childrenEat.getId(), childrenEat.getName(), childrenEat.getPrice());
         basketService.saveMyorder(basketChildrenFood);
         return "redirect:/menu";
     }
 
 
-
-
-
     //удалить по id   c корзины
     @RequestMapping("/deleteMyList/{id}")
-    public String deleteMyListById(@PathVariable (name = "id") Long id){
+    public String deleteMyListById(@PathVariable(name = "id") Long id) {
         basketService.deleteById(id);
         return "redirect:/menu";
 
     }
+
 }
+
--- src/main/java/com/example/restorans/service/BasketService.java
+++ src/main/java/com/example/restorans/service/BasketService.java
@@ -3,14 +3,20 @@ package com.example.restorans.service;
 import com.example.restorans.model.Basket;
 import com.example.restorans.repository.BasketRepository;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Setter
+@Getter
 public class BasketService {
     private final BasketRepository basketRepository ;
+
 
 
     public void saveMyorder(Basket basket){
@@ -24,6 +30,23 @@ public class BasketService {
 
     public void deleteById(Long id){
         basketRepository.deleteById(id);
+    }
+
+
+
+    private List<Basket> orders;
+
+    // Другие поля и методы корзины
+
+    public BigDecimal calculateTotalOrderCost() {
+        BigDecimal totalCost = BigDecimal.ZERO; // Начальное значение общей стоимости
+
+        // Проходимся по всем заказам в корзине и добавляем их стоимость к общей стоимости
+        for (Basket order : basketRepository.findAll()) {
+            totalCost = totalCost.add(order.getPrice());
+        }
+
+        return totalCost;
     }
 
 
